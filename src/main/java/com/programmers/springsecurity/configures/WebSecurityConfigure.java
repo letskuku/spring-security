8514f1aaf154f2cc1d1146f0d72c64e0b6e30992
letskuku <yujung9288@naver.com>
2023-11-22 13:31:40 +0900

    init: first commit

---
+++ src/main/java/com/programmers/springsecurity/configures/WebSecurityConfigure.java
@@ -0,0 +1,31 @@
+package com.programmers.springsecurity.configures;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/assets/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin(login -> login
+                        .defaultSuccessUrl("/")
+                        .permitAll());
+        return http.build();
+    }
+}

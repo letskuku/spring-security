391000fdd99129cbc49a2224716db1331bc9c03d
letskuku <yujung9288@naver.com>
2023-11-28 01:24:58 +0900

    feat: ADMIN 권한만 허용하는 페이지 추가 및 인가 설정

--- src/main/java/com/programmers/springsecurity/configures/WebSecurityConfigure.java
+++ src/main/java/com/programmers/springsecurity/configures/WebSecurityConfigure.java
@@ -1,19 +1,27 @@
 package com.programmers.springsecurity.configures;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -38,10 +46,25 @@ public class WebSecurityConfigure {
     }
 
     @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, e) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+            log.warn("{} is denied", principal, e);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain;charset=UTF-8");
+            response.getWriter().write("ACCESS DENIED");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/me").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().permitAll())
                 .formLogin(login -> login
                         .defaultSuccessUrl("/")
@@ -56,7 +79,9 @@ public class WebSecurityConfigure {
                         .clearAuthentication(true))
                 .requiresChannel(require -> require
                         .anyRequest()
-                        .requiresSecure());
+                        .requiresSecure())
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler(accessDeniedHandler()));
         return http.build();
     }
 }

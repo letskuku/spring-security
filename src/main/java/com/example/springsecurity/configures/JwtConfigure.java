8f3075bada7b8c2c12fc842e0f3c7c83b082ad96
letskuku <yujung9288@naver.com>
2023-12-06 03:22:38 +0900

    chore: 필요없는 코드 및 파일 정리

--- src/main/java/com/example/springsecurity/configures/JwtConfigure.java
+++ src/main/java/com/example/springsecurity/configures/JwtConfigure.java
@@ -5,8 +5,6 @@ import com.example.springsecurity.jwt.JwtProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class JwtConfigure {
@@ -25,10 +23,5 @@ public class JwtConfigure {
                 jwtProperties.getClientSecret(),
                 jwtProperties.getExpirySeconds()
         );
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
